2c44f2d484f53853b0e516a7226b70b50cff9ebd
Emma Gouti <goutiemma@gmail.com>
2018-10-03 22:45:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |string|
-    string ==
+    cheese_types.any?(string)
+  end
   
 end
