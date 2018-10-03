bc629a654f96e6bccf22cbc6883a183eea216b83
Emma Gouti <goutiemma@gmail.com>
2018-10-03 22:42:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,8 @@ def long_planeteer_calls(array)
     
 end
 
-def find_the_cheese()
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
